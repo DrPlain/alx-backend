9b1ab7c7435ba54d2ac53202b4745d8cb5598f23
Gideon Obiasor <gideonoiasor@gmail.com>
2023-05-16 14:02:13 +0100

    task 0-4

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" LIFOCache class """
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFO Cache class """
+    track_key = []
+
+    def __init__(self):
+        """ class constructor """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Adds an item to cache """
+        if key or item:
+            if len(self.cache_data) < BaseCaching.MAX_ITEMS:
+                self.cache_data[key] = item
+                LIFOCache.track_key.append(key)
+            else:
+                if key in self.cache_data.keys():
+                    self.cache_data[key] = item
+                    LIFOCache.track_key.append(key)
+                else:
+                    last_key = LIFOCache.track_key[-1]
+                    del self.cache_data[last_key]
+                    self.cache_data[key] = item
+                    LIFOCache.track_key.append(key)
+                    print(f"DISCARD: {last_key}")
---
+++ 0x01-caching/README.md
@@ -0,0 +1,6 @@
+# Caching policies
+## First In First Out - FIFO
+## Last In First Out - LIFO
+## Least Recently Used - LRU
+## Most Recently USed - MRU
+## Least-Frequently Used - LFU
